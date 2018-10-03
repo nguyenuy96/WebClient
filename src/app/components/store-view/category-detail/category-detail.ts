97e92576ed6aabb4257cb07da4e1ceff66c3dfde
uy <nguyenuy710@gmail.com>
2018-10-03 07:41:45 +0700

    03/10

--- src/app/components/store-view/category-detail/category-detail.ts
+++ src/app/components/store-view/category-detail/category-detail.ts
@@ -1,9 +1,21 @@
 import { Component } from "@angular/core";
-
+interface Product {
+    name: string;
+    image: string;
+    price: string;
+}
 @Component({
     selector: 'category-detail',
     templateUrl: './category-detail.html',
     styleUrls: ['./category-detail.css']
 })
-
-export class ProductCategoryDetail { }
+export class ProductCategoryDetail {
+    products: Product[] = [
+        { name: 'NÓN PHÙ THỦY', image: '', price: '3200' },
+        { name: 'ĐỒNG HỒ CÁT', image: '', price: '3200' },
+        { name: 'DÂY CHUYỀN CHỮ THẬP', image: '', price: '3800' },
+        { name: 'GƯƠM CỦA VUA VÔ DANH', image: '', price: '3600' },
+        { name: 'HUYẾT KIẾM', image: '', price: '3800' },
+        { name: 'VÔ CỰC KIẾM', image: '', price: '3100' }
+    ]
+}
